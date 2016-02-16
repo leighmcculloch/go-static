5aea57ef500cda97f98507e7e0ba66bb824ded6d
Leigh McCulloch <leigh@mcchouse.com>
2016-02-15 18:18:07 -0800

    Minor refactor of static_serve.go functions.
    What
    ===
    Minor refactor of static_serve.go functions by taking the one big
    function and breaking it up.
    
    Why
    ===
    It was a little big.

--- static_serve.go
+++ static_serve.go
@@ -6,25 +6,29 @@ import (
 	"strings"
 )
 
-func (s *Static) ListenAndServe(addr string, eventHandler EventHandler) error {
-	fileServer := http.FileServer(http.Dir(s.BuildDir))
+func (s *Static) ListenAndServe(addr string, ev EventHandler) error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path
-
-		filePath := path
-		if strings.HasSuffix(path, "/") {
-			filePath = fmt.Sprintf("%sindex.html", path)
-		}
-
-		err := s.handleRequest(w, filePath, true)
-
-		if err == errNotFound {
-			fileServer.ServeHTTP(w, r)
-			eventHandler(Event{Action: "file", Path: path})
-		} else {
-			eventHandler(Event{Action: "build", Path: filePath, Error: err})
-		}
+		s.serverHandleRequest(w, r, ev)
 	})
 	return http.ListenAndServe(addr, mux)
+}
+
+func (s *Static) serverHandleRequest(w http.ResponseWriter, r *http.Request, ev EventHandler) {
+	path := r.URL.Path
+
+	filePath := path
+	if strings.HasSuffix(path, "/") {
+		filePath = fmt.Sprintf("%sindex.html", path)
+	}
+
+	err := s.handleRequest(w, filePath, true)
+
+	if err == errNotFound {
+		fileServer := http.FileServer(http.Dir(s.BuildDir))
+		fileServer.ServeHTTP(w, r)
+		ev(Event{Action: "file", Path: path})
+	} else {
+		ev(Event{Action: "build", Path: filePath, Error: err})
+	}
 }
