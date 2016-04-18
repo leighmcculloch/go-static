5a246f65516273fed3b99e44eca6a368676b594a
Leigh McCulloch <leigh@mcchouse.com>
2016-04-17 22:35:18 -0700

    Added a `String()` implementation to `Event`.
    What
    ===
    Added a `String()` implementation to `Event`.
    
    Why
    ===
    So that users of this library can log events simply using a standard
    format if they wish.

--- event.go
+++ event.go
@@ -1,7 +1,19 @@
 package static
 
+import (
+	"fmt"
+)
+
 type Event struct {
 	Action string
 	Path   string
 	Error  error
+}
+
+func (e Event) String() string {
+	s := fmt.Sprintf("%10s  %-20s", e.Action, e.Path)
+	if e.Error != nil {
+		s += fmt.Sprintf(" Error: %v", e.Error)
+	}
+	return s
 }
