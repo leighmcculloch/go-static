05046fb8925cc94ab3a2ffc54f54d9a8b2861c5f
Leigh McCulloch <leigh@mcchouse.com>
2016-04-19 23:24:27 -0700

    Added inline documentation.
    What
    ===
    Added inline documentation to all remaining undocumented exported files.
    
    Why
    ===
    For generated go docs.

--- event.go
+++ event.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 )
 
+// A build event representing that an action has taken place for a path in the build process, and includes an error if an error occurred while the action took place.
 type Event struct {
+	// The action taken place on the path.
 	Action string
-	Path   string
-	Error  error
+	// The path the action has taken place on.
+	Path string
+	// An error if an error occurred while performing the action, otherwise nil.
+	Error error
 }
 
+// A simple string representation of an error in the format: `Action: <action>, Path: <path>, Error: <error>`. Where the error portion is only included if an error occurred.
 func (e Event) String() string {
 	s := fmt.Sprintf("Action: %s, Path: %s", e.Action, e.Path)
 	if e.Error != nil {
--- options.go
+++ options.go
@@ -2,15 +2,15 @@ package static
 
 // The options for configuring the behavior of the Build functions. Get the default options by calling DefaultOptions().
 type Options struct {
-	// The directory where files will be written when building, default "build".
+	// The directory where files will be written when building.
 	OutputDir string
-	// The number of files that will be built concurrently, default 50.
+	// The number of files that will be built concurrently.
 	Concurrency int
-	// The filename to save director paths to, default "index.html".
+	// The filename to save director paths to.
 	DirFilename string
 }
 
-// The default Options.
+// The default Options: OutputDir: "build", Concurrency: 50, DirFilename: "index.html".
 func DefaultOptions() Options {
 	return Options{
 		OutputDir:   "build",
