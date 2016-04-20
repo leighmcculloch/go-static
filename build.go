b67af40e795dfcbf588060f59558e30fc97513a8
Leigh McCulloch <leigh@mcchouse.com>
2016-04-19 23:08:06 -0700

    Added inline documentation.
    What
    ===
    Added inline documentation.
    
    Why
    ===
    For the go docs.

--- build.go
+++ build.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 )
 
+// Build the paths. Uses the http.Handler to get the response for each path, and writes that response to a file with it's respective path in the OutputDir specified in the Options. Does so concurrently as defined in the Options, and calls the EventHandler for every path with an Event that states that the path was built and if an error occurred.
 func Build(o Options, h http.Handler, paths []string, eh EventHandler) {
 	var wg sync.WaitGroup
 
@@ -16,7 +17,7 @@ func Build(o Options, h http.Handler, paths []string, eh EventHandler) {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			buildPaths(o, h, pathsChan, eh)
+			buildWorker(o, h, pathsChan, eh)
 		}()
 	}
 
@@ -29,7 +30,7 @@ func Build(o Options, h http.Handler, paths []string, eh EventHandler) {
 	wg.Wait()
 }
 
-func buildPaths(o Options, h http.Handler, paths <-chan string, eh EventHandler) {
+func buildWorker(o Options, h http.Handler, paths <-chan string, eh EventHandler) {
 	for path := range paths {
 		err := BuildSingle(o, h, path)
 		if eh != nil {
@@ -38,6 +39,7 @@ func buildPaths(o Options, h http.Handler, paths <-chan string, eh EventHandler)
 	}
 }
 
+// Build a single path. Uses the http.Handler to get the response for each path, and writes that response to a file with it's respective path in the OutputDir specified in the Options. Returns an error if one occurs.
 func BuildSingle(o Options, h http.Handler, path string) error {
 	_, err := os.Stat(o.OutputDir)
 	if os.IsNotExist(err) {
