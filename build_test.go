c769f2ffd99a86bb94c6ff45984f525e8f7dbc23
Leigh McCulloch <leigh@mcchouse.com>
2016-04-20 23:49:47 -0700

    Add a happy case test for `BuildSingle`.
    What
    ===
    Add a happy case test for `BuildSingle`.
    
    Why
    ===
    Tests.

--- build_test.go
+++ build_test.go
@@ -3,7 +3,10 @@ package static_test
 import (
 	"."
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
+	"testing"
 )
 
 func ExampleBuild() {
@@ -38,4 +41,40 @@ func ExampleBuildSingle() {
 
 	// Output:
 	// Built: /world, Error: <nil>
+}
+
+func TestBuildSingle(t *testing.T) {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %s!", r.URL.Path)
+	})
+
+	options := static.DefaultOptions()
+	tempDir, err := ioutil.TempDir("", "test_build_single")
+	if err != nil {
+		t.Fatalf("Error creating temp dir for OutputDir: %v", err)
+	}
+	t.Logf("Created temp dir for OutputDir: %s", tempDir)
+	options.OutputDir = filepath.Join(tempDir, "build")
+	t.Logf("OutputDir: %s", options.OutputDir)
+
+	path := "/world"
+	expectedOutputFilePath := filepath.Join(options.OutputDir, "world")
+	expectedOutputFileContents := "Hello /world!"
+
+	err = static.BuildSingle(options, handler, path)
+	t.Logf("BuildSingle(..., %#v) => %v", path, err)
+	if err != nil {
+		t.Errorf("BuildSingle(..., %#v) => %v, want nil", path, err)
+	}
+
+	outputFileContents, err := ioutil.ReadFile(expectedOutputFilePath)
+	if err != nil {
+		t.Fatalf("Expected %s to exist with the output but got error when opening: %v", expectedOutputFilePath, err)
+	}
+
+	t.Logf("Contents of %s => %s", expectedOutputFilePath, outputFileContents)
+	if string(outputFileContents) != expectedOutputFileContents {
+		t.Fatalf(`Contents of %s => %s, want %s`, expectedOutputFilePath, outputFileContents, expectedOutputFileContents)
+	}
 }
