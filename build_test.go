0175e3eaf3e4757f05e25ac3379f46dbd53b5189
Leigh McCulloch <leigh@mcchouse.com>
2016-04-22 00:05:06 -0700

    Added tests for `Build`.
    What
    ===
    Added tests for `Build`.
    
    Why
    ===
    Tests

--- build_test.go
+++ build_test.go
@@ -78,3 +78,106 @@ func TestBuildSingle(t *testing.T) {
 		t.Fatalf(`Contents of %s => %s, want %s`, expectedOutputFilePath, outputFileContents, expectedOutputFileContents)
 	}
 }
+
+func TestBuild(t *testing.T) {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %s!", r.URL.Path)
+	})
+
+	options := static.DefaultOptions()
+	tempDir, err := ioutil.TempDir("", "test_build")
+	if err != nil {
+		t.Fatalf("Error creating temp dir for OutputDir: %v", err)
+	}
+	t.Logf("Created temp dir for OutputDir: %s", tempDir)
+	options.OutputDir = filepath.Join(tempDir, "build")
+	t.Logf("OutputDir: %s", options.OutputDir)
+
+	paths := []string{"/go", "/world", "/universe"}
+	expected := []struct {
+		OutputFilePath     string
+		OutputFileContents string
+		Event              static.Event
+	}{
+		{filepath.Join(options.OutputDir, "go"), "Hello /go!", static.Event{"build", "/go", nil}},
+		{filepath.Join(options.OutputDir, "world"), "Hello /world!", static.Event{"build", "/world", nil}},
+		{filepath.Join(options.OutputDir, "universe"), "Hello /universe!", static.Event{"build", "/universe", nil}},
+	}
+
+	expectedNumberOfEvents := len(paths)
+	events := make(chan static.Event, expectedNumberOfEvents)
+	static.Build(options, handler, paths, func(e static.Event) {
+		select {
+		case events <- e:
+		default:
+			t.Errorf("Event %#v was seen, but we have already seen %d events and didn't expect anymore.", e, expectedNumberOfEvents)
+		}
+	})
+	close(events)
+
+	eventsSeen := make(map[static.Event]bool)
+	for event := range events {
+		t.Logf("Event %#v was seen.", event)
+		if eventsSeen[event] {
+			t.Errorf("Event %#v was seen more than once, but was only expected once.", event)
+		}
+		eventsSeen[event] = true
+	}
+
+	for _, expect := range expected {
+		if !eventsSeen[expect.Event] {
+			t.Errorf("Event %#v was not seen during the build.", expect.Event)
+		}
+
+		outputFileContents, err := ioutil.ReadFile(expect.OutputFilePath)
+		if err != nil {
+			t.Fatalf("Expected %s to exist with the output but got error when opening: %v", expect.OutputFilePath, err)
+		}
+
+		t.Logf("Contents of %s => %s", expect.OutputFilePath, outputFileContents)
+		if string(outputFileContents) != expect.OutputFileContents {
+			t.Fatalf(`Contents of %s => %s, want %s`, expect.OutputFilePath, outputFileContents, expect.OutputFileContents)
+		}
+	}
+}
+
+func TestBuild_WithoutEventHandler(t *testing.T) {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %s!", r.URL.Path)
+	})
+
+	options := static.DefaultOptions()
+	tempDir, err := ioutil.TempDir("", "test_build")
+	if err != nil {
+		t.Fatalf("Error creating temp dir for OutputDir: %v", err)
+	}
+	t.Logf("Created temp dir for OutputDir: %s", tempDir)
+	options.OutputDir = filepath.Join(tempDir, "build")
+	t.Logf("OutputDir: %s", options.OutputDir)
+
+	paths := []string{"/go", "/world", "/universe"}
+	expected := []struct {
+		OutputFilePath     string
+		OutputFileContents string
+	}{
+		{filepath.Join(options.OutputDir, "go"), "Hello /go!"},
+		{filepath.Join(options.OutputDir, "world"), "Hello /world!"},
+		{filepath.Join(options.OutputDir, "universe"), "Hello /universe!"},
+	}
+
+	static.Build(options, handler, paths, nil)
+
+	for _, expect := range expected {
+		outputFileContents, err := ioutil.ReadFile(expect.OutputFilePath)
+		if err != nil {
+			t.Fatalf("Expected %s to exist with the output but got error when opening: %v", expect.OutputFilePath, err)
+		}
+
+		t.Logf("Contents of %s => %s", expect.OutputFilePath, outputFileContents)
+		if string(outputFileContents) != expect.OutputFileContents {
+			t.Fatalf(`Contents of %s => %s, want %s`, expect.OutputFilePath, outputFileContents, expect.OutputFileContents)
+		}
+	}
+}
