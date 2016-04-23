279f64fea92b34df927d10ed40da27405f258c28
Leigh McCulloch <leigh@mcchouse.com>
2016-04-22 20:54:38 -0700

    Tidy and include more context in complex tests.
    What
    ===
    Tidy and include more context in complex tests.
    
    Why
    ===
    The tests were complex and I'm learning that go tests should describe
    themselves as they run.

--- build_test.go
+++ build_test.go
@@ -45,25 +45,29 @@ func ExampleBuildSingle() {
 }
 
 func TestBuildSingle(t *testing.T) {
+	t.Log("When a Handler is defined to respond to /* and response with Hello <path>!")
 	handler := http.NewServeMux()
 	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello %s!", path.Base(r.URL.Path))
 	})
 
+	t.Log("And Options are defined with defaults and an OutputDir that does not exist.")
 	options := static.DefaultOptions()
-	tempDir, _ := ioutil.TempDir("", "test_build_single")
-	t.Logf("Created temp dir for OutputDir: %s", tempDir)
+	tempDir, _ := ioutil.TempDir("", "")
 	options.OutputDir = filepath.Join(tempDir, "build")
-	t.Logf("OutputDir: %s", options.OutputDir)
+	t.Logf("OutputDir => %s", options.OutputDir)
 
+	t.Log("And the path to build is /world.")
 	path := "/world"
+
+	t.Log("Expect BuildSingle to create the OutputDir, write a file world with contents Hello world! And return no error.")
 	expectedOutputFilePath := filepath.Join(options.OutputDir, "world")
 	expectedOutputFileContents := "Hello world!"
 
 	err := static.BuildSingle(options, handler, path)
 	t.Logf("BuildSingle(..., %#v) => %v", path, err)
 	if err != nil {
-		t.Errorf("BuildSingle(..., %#v) => %v, want nil", path, err)
+		t.Errorf("BuildSingle(..., %#v) => %v, expected nil", path, err)
 	}
 
 	outputFileContents, err := ioutil.ReadFile(expectedOutputFilePath)
@@ -73,82 +77,109 @@ func TestBuildSingle(t *testing.T) {
 
 	t.Logf("Contents of %s => %s", expectedOutputFilePath, outputFileContents)
 	if string(outputFileContents) != expectedOutputFileContents {
-		t.Fatalf(`Contents of %s => %s, want %s`, expectedOutputFilePath, outputFileContents, expectedOutputFileContents)
+		t.Fatalf(`Contents of %s => %s, expected %s`, expectedOutputFilePath, outputFileContents, expectedOutputFileContents)
 	}
 }
 
 func TestBuild(t *testing.T) {
+	t.Log("When a Handler is defined to respond to /* and response with Hello <path>!")
 	handler := http.NewServeMux()
 	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello %s!", path.Base(r.URL.Path))
 	})
 
+	t.Log("And Options are defined with defaults and an OutputDir that does not exist.")
 	options := static.DefaultOptions()
-	tempDir, _ := ioutil.TempDir("", "test_build")
+	tempDir, _ := ioutil.TempDir("", "")
 	options.OutputDir = filepath.Join(tempDir, "build")
 	t.Logf("OutputDir: %s", options.OutputDir)
 
+	t.Log("And there are multiple paths to build.")
 	paths := []string{"/go", "/world", "/universe"}
+
+	t.Log("Expect Build to create the OutputDir, write a file for each path with contents Hello <file>! And send one event per path to the EventHandler containing no error.")
 	expected := []struct {
 		OutputFilePath     string
 		OutputFileContents string
 		Event              static.Event
 	}{
-		{filepath.Join(options.OutputDir, "go"), "Hello go!", static.Event{"build", "/go", nil}},
-		{filepath.Join(options.OutputDir, "world"), "Hello world!", static.Event{"build", "/world", nil}},
-		{filepath.Join(options.OutputDir, "universe"), "Hello universe!", static.Event{"build", "/universe", nil}},
+		{
+			filepath.Join(options.OutputDir, "go"),
+			"Hello go!",
+			static.Event{"build", "/go", nil},
+		},
+		{
+			filepath.Join(options.OutputDir, "world"),
+			"Hello world!",
+			static.Event{"build", "/world", nil},
+		},
+		{
+			filepath.Join(options.OutputDir, "universe"),
+			"Hello universe!",
+			static.Event{"build", "/universe", nil},
+		},
 	}
 
 	expectedNumberOfEvents := len(paths)
+	numberOfEvents := 0
 	events := make(chan static.Event, expectedNumberOfEvents)
 	static.Build(options, handler, paths, func(e static.Event) {
 		select {
 		case events <- e:
+			t.Logf("Event received => %#v", e)
 		default:
-			t.Errorf("Event %#v was seen, but we have already seen %d events and didn't expect anymore.", e, expectedNumberOfEvents)
+			t.Errorf("Additional event received => %#v", numberOfEvents, expectedNumberOfEvents)
 		}
+		numberOfEvents += 1
 	})
 	close(events)
+	if numberOfEvents != expectedNumberOfEvents {
+		t.Errorf("Number of events received => %d, expected %d", numberOfEvents, expectedNumberOfEvents)
+	}
 
 	eventsSeen := make(map[static.Event]bool)
 	for event := range events {
-		t.Logf("Event %#v was seen.", event)
 		if eventsSeen[event] {
-			t.Errorf("Event %#v was seen more than once, but was only expected once.", event)
+			t.Errorf("Event received => %#v, multiple times but was expected once.", event)
 		}
 		eventsSeen[event] = true
 	}
 
 	for _, expect := range expected {
 		if !eventsSeen[expect.Event] {
-			t.Errorf("Event %#v was not seen during the build.", expect.Event)
+			t.Errorf("Event not received => %#v, but was expected once.", expect.Event)
 		}
 
 		outputFileContents, err := ioutil.ReadFile(expect.OutputFilePath)
 		if err != nil {
-			t.Fatalf("Expected %s to exist with the output but got error when opening: %v", expect.OutputFilePath, err)
+			t.Fatalf("Error opening output file => %#v, expected to exist.", err)
 		}
 
-		t.Logf("Contents of %s => %s", expect.OutputFilePath, outputFileContents)
-		if string(outputFileContents) != expect.OutputFileContents {
-			t.Fatalf(`Contents of %s => %s, want %s`, expect.OutputFilePath, outputFileContents, expect.OutputFileContents)
+		if string(outputFileContents) == expect.OutputFileContents {
+			t.Logf("Contents of %s => %s", expect.OutputFilePath, outputFileContents)
+		} else {
+			t.Fatalf(`Contents of %s => %s, expected %s`, expect.OutputFilePath, outputFileContents, expect.OutputFileContents)
 		}
 	}
 }
 
-func TestBuild_WithoutEventHandler(t *testing.T) {
+func TestBuildWithNilEventHandler(t *testing.T) {
+	t.Log("When a Handler is defined to respond to /* and response with Hello <path>!")
 	handler := http.NewServeMux()
 	handler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello %s!", path.Base(r.URL.Path))
 	})
 
+	t.Log("And Options are defined with defaults and an OutputDir that does not exist.")
 	options := static.DefaultOptions()
-	tempDir, _ := ioutil.TempDir("", "test_build_without_event_handler")
-	t.Logf("Created temp dir for OutputDir: %s", tempDir)
+	tempDir, _ := ioutil.TempDir("", "")
 	options.OutputDir = filepath.Join(tempDir, "build")
 	t.Logf("OutputDir: %s", options.OutputDir)
 
+	t.Log("And there are multiple paths to build.")
 	paths := []string{"/go", "/world", "/universe"}
+
+	t.Log("Expect Build to create the OutputDir, write a file for each path with contents Hello <file>!")
 	expected := []struct {
 		OutputFilePath     string
 		OutputFileContents string
@@ -163,12 +194,13 @@ func TestBuild_WithoutEventHandler(t *testing.T) {
 	for _, expect := range expected {
 		outputFileContents, err := ioutil.ReadFile(expect.OutputFilePath)
 		if err != nil {
-			t.Fatalf("Expected %s to exist with the output but got error when opening: %v", expect.OutputFilePath, err)
+			t.Fatalf("Error opening output file => %#v, expected to exist.", err)
 		}
 
-		t.Logf("Contents of %s => %s", expect.OutputFilePath, outputFileContents)
-		if string(outputFileContents) != expect.OutputFileContents {
-			t.Fatalf(`Contents of %s => %s, want %s`, expect.OutputFilePath, outputFileContents, expect.OutputFileContents)
+		if string(outputFileContents) == expect.OutputFileContents {
+			t.Logf("Contents of %s => %s", expect.OutputFilePath, outputFileContents)
+		} else {
+			t.Fatalf(`Contents of %s => %s, expected %s`, expect.OutputFilePath, outputFileContents, expect.OutputFileContents)
 		}
 	}
 }
