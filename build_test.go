9f5d707c569e0ef50d6f5e5eeee0c97573afdeed
Leigh McCulloch <leigh@mcchouse.com>
2016-04-22 22:47:21 -0700

    Add tests for errors during builds.
    What
    ===
    Add tests for errors during builds.
    
    Why
    ===
    Tests.

--- build_test.go
+++ build_test.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"path"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -65,9 +67,9 @@ func TestBuildSingle(t *testing.T) {
 	expectedOutputFileContents := "Hello world!"
 
 	err := static.BuildSingle(options, handler, path)
-	t.Logf("BuildSingle(..., %#v) => %v", path, err)
+	t.Logf("BuildSingle(%#v) => %v", path, err)
 	if err != nil {
-		t.Errorf("BuildSingle(..., %#v) => %v, expected nil", path, err)
+		t.Errorf("BuildSingle(%#v) => %v, expected nil", path, err)
 	}
 
 	outputFileContents, err := ioutil.ReadFile(expectedOutputFilePath)
@@ -78,6 +80,37 @@ func TestBuildSingle(t *testing.T) {
 	t.Logf("Contents of %s => %s", expectedOutputFilePath, outputFileContents)
 	if string(outputFileContents) != expectedOutputFileContents {
 		t.Fatalf(`Contents of %s => %s, expected %s`, expectedOutputFilePath, outputFileContents, expectedOutputFileContents)
+	}
+}
+
+func TestBuildSingleErrors(t *testing.T) {
+	t.Log("When a Handler is defined to respond to /* and response with Hello <path>!")
+	handler := http.NewServeMux()
+	handler.HandleFunc("/hello/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %s!", path.Base(r.URL.Path))
+	})
+
+	t.Log("And Options are defined with defaults and an OutputDir.")
+	options := static.DefaultOptions()
+	tempDir, _ := ioutil.TempDir("", "")
+	options.OutputDir = filepath.Join(tempDir, "build")
+	t.Logf("OutputDir => %s", options.OutputDir)
+
+	t.Log("And a file exists at the same path as the OutputDir (a problem).")
+	f, _ := os.Create(options.OutputDir)
+	defer f.Close()
+
+	t.Log("And the path to build is /hello/world.")
+	path := "/hello/world"
+
+	t.Log("Expect BuildSingle to error with not a directory error.")
+
+	expectedErrString := "not a directory"
+	err := static.BuildSingle(options, handler, path)
+	if err != nil && strings.Contains(err.Error(), expectedErrString) {
+		t.Logf("BuildSingle(%#v) => %v", path, err)
+	} else {
+		t.Errorf("BuildSingle(%#v) => %v, expected a %s error", path, err, expectedErrString)
 	}
 }
 
@@ -163,6 +196,82 @@ func TestBuild(t *testing.T) {
 			t.Logf("Contents of %s => %s", expect.OutputFilePath, outputFileContents)
 		} else {
 			t.Fatalf(`Contents of %s => %s, expected %s`, expect.OutputFilePath, outputFileContents, expect.OutputFileContents)
+		}
+	}
+}
+
+func TestBuildErrors(t *testing.T) {
+	t.Log("When a Handler is defined to respond to /* and response with Hello <path>!")
+	handler := http.NewServeMux()
+	handler.HandleFunc("/hello/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %s!", path.Base(r.URL.Path))
+	})
+
+	t.Log("And Options are defined with defaults and an OutputDir that does not exist.")
+	options := static.DefaultOptions()
+	tempDir, _ := ioutil.TempDir("", "")
+	options.OutputDir = filepath.Join(tempDir, "build")
+	t.Logf("OutputDir: %s", options.OutputDir)
+
+	t.Log("And a file exists at the same path as the OutputDir (a problem).")
+	f, _ := os.Create(options.OutputDir)
+	defer f.Close()
+
+	t.Log("And there are multiple paths to build.")
+	paths := []string{
+		"/hello/go",
+		"/hello/world",
+		"/hello/universe",
+	}
+
+	t.Log("Expect Build to send one event per path to the EventHandler containing a not a directory error.")
+
+	expectedNumberOfEvents := len(paths)
+	numberOfEvents := 0
+	events := make(chan static.Event, expectedNumberOfEvents)
+	static.Build(options, handler, paths, func(e static.Event) {
+		select {
+		case events <- e:
+			t.Logf("Event received => %#v", e)
+		default:
+			t.Errorf("Additional event received => %#v", numberOfEvents, expectedNumberOfEvents)
+		}
+		numberOfEvents += 1
+	})
+	close(events)
+	if numberOfEvents != expectedNumberOfEvents {
+		t.Errorf("Number of events received => %d, expected %d", numberOfEvents, expectedNumberOfEvents)
+	}
+
+	eventsSeen := make(map[string]*static.Event)
+	for event := range events {
+		storeEvent := event
+		if eventsSeen[event.Path] != nil {
+			t.Errorf("Event received => %#v, multiple times but was expected once.", event)
+		}
+		eventsSeen[event.Path] = &storeEvent
+	}
+
+	for _, path := range paths {
+		event := eventsSeen[path]
+		if event == nil {
+			t.Errorf("Event not received for path => %s, but was expected once.", path)
+		}
+
+		expectedAction := "build"
+		if event.Action != expectedAction {
+			t.Errorf("Event for %s Action => %s, expected %s.", path, event.Action, expectedAction)
+		}
+
+		if event.Path != path {
+			t.Errorf("Event for %s Path => %s, expected %s.", path, event.Path, path)
+		}
+
+		expectedErrString := "not a directory"
+		if event.Error != nil && strings.Contains(event.Error.Error(), expectedErrString) {
+			t.Logf("Build(%#v) => %v", path, event.Error)
+		} else {
+			t.Errorf("Build(%#v) => %v, expected a %s error", path, event.Error, expectedErrString)
 		}
 	}
 }
