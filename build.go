051c6893395bbf02986f9cc43a2def78eb6355a0
Leigh McCulloch <leigh@mcchouse.com>
2016-05-03 23:09:08 -0700

    Swapped nil checks for no-op defaults.
    What
    ===
    Swapped nil checks on EventHandler for a no-op default event handler
    that does nothing.
    
    Why
    ===
    The Go Best Practices 2016 article linked below, recommended applying
    sensible defaults over testing code for nils. By doing so code that uses
    the event handler does not need to be concerned with whether it is
    available or not, if one is not needed it can be an empty function.
    
    https://peter.bourgon.org/go-best-practices-2016/#program-design
    
    This may allow for compiler optimization since the compiler may know that
    a variable is always nil and can remove out those calls. I don't know
    that for sure but I'd be interested to find out.
    
    Signed-off-by: Leigh McCulloch <leigh@mcchouse.com>

--- build.go
+++ build.go
@@ -9,8 +9,12 @@ import (
 	"sync"
 )
 
-// Build the paths. Uses the http.Handler to get the response for each path, and writes that response to a file with it's respective path in the OutputDir specified in the Options. Does so concurrently as defined in the Options, and calls the EventHandler for every path with an Event that states that the path was built and if an error occurred.
+// Build the paths. Uses the http.Handler to get the response for each path, and writes that response to a file with it's respective path in the OutputDir specified in the Options. Does so concurrently as defined in the Options, and calls the EventHandler for every path with an Event that states that the path was built and if an error occurred. EventHandler may be nil.
 func Build(o Options, h http.Handler, paths []string, eh EventHandler) {
+	if eh == nil {
+		eh = defaultEventHandler
+	}
+
 	var wg sync.WaitGroup
 
 	pathsChan := make(chan string)
@@ -35,9 +39,7 @@ func Build(o Options, h http.Handler, paths []string, eh EventHandler) {
 func buildWorker(o Options, h http.Handler, paths <-chan string, eh EventHandler) {
 	for path := range paths {
 		status, err := BuildSingle(o, h, path)
-		if eh != nil {
-			eh(Event{Action: "build", StatusCode: status, Path: path, Error: err})
-		}
+		eh(Event{Action: "build", StatusCode: status, Path: path, Error: err})
 	}
 }
 
